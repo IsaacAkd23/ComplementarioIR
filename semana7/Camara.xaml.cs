4a0a6a63f4c843d64b9f52dc072824aa459a15f4
e1751250398 <e1751250398@uisrael.edu.ec>
2023-02-02 21:22:54 -0500

    Agregar archivos de proyecto.

---
+++ semana7/Camara.xaml.cs
@@ -0,0 +1,57 @@
+﻿using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace semana7
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Camara : ContentPage
+    {
+        public Camara()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnCamara_Clicked(object sender, EventArgs e)
+        {
+            var opciones_almacenamiento = new StoreCameraMediaOptions()
+            {
+                SaveToAlbum = true,
+                Name = "Mi foto.jpg"
+            };
+            var foto = await CrossMedia.Current.TakePhotoAsync(opciones_almacenamiento);
+            MiImagen.Source = ImageSource.FromStream(() =>
+            {
+                var stream = foto.GetStream();
+                foto.Dispose();
+                return stream;
+            });
+
+
+        }
+
+        private async void btnSeleccionar_Clicked(object sender, EventArgs e)
+        {
+            if(CrossMedia.Current.IsTakePhotoSupported)
+            {
+                var imagen = await CrossMedia.Current.PickVideoAsync();
+                if(imagen != null)
+                {
+                    MiImagen.Source = ImageSource.FromStream(() =>
+                    {
+                        var stream = imagen.GetStream();
+                        imagen.Dispose();
+                        return stream;
+                    });
+                }
+            }
+        }
+    }
+}
